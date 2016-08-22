b962c39dbe63abdfe4e0e47f7c98d4e1e69f9e38
Bobby Crowston and Ryan Apking <ryan@>
2016-08-22 15:25:20 -0700

    added so many things

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,22 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var itemInput = $("#itemInput").val();
+    if(itemInput.length === 0){
+      alert("Please enter something");
+      return;
+    }
     var important = $("#importantCheck").is(':checked');
     var note = $("#noteInput").val();
     var newTask = new Task(itemInput, important, note);
-    var listItemString = "<span class='item'>" + newTask.item + "  </span>";
-    if (note!== "") {
-      listItemString = listItemString + "<span class='arrow'>  notes ></span>";
-      listItemString = listItemString + "<br><span class='note hidden'>" + newTask.note + "</span>";
-    }
+    var listItemString = "<span class='item'>" + newTask.item + "</span>";
+    listItemString = listItemString + "<span class='arrow hidden'>     show note &#9660;</span>";
+    listItemString = listItemString + "<br><span class='note hidden'>" + newTask.note + "<br></span>";
     $("#results").append("<li>" + listItemString + "</li>");
+
+    if (note) {
+      debugger;
+      $(".arrow").last().removeClass("hidden");
+    }
 
     $("#itemInput").val("");
     $("#noteInput").val("");
@@ -28,24 +35,63 @@ $(document).ready(function(){
     };
 
     $(".item").last().click(function() {
-      if ($(this).hasClass("strikethrough")) {
-        if (confirm("Then why did you cross it off?!?! Are you sure?")) {
-          $(this).removeClass("strikethrough");
+
+      if ($("html").hasClass("noteCursor")) {
+        var newNote = prompt("Enter your note");
+        if (newNote) {
+          $(this).parent().children(".note").append(newNote + "<br>");
+          $(this).parent().children(".arrow").removeClass("hidden");
         }
+        $("html").removeClass("noteCursor");
+        // debugger;
+      } else if ($("html").hasClass("deleteCursor")) {
+        $(this).parent().remove();
+        $("html").removeClass("deleteCursor");
+        $(".instructions").hide();
       } else {
-        $(this).addClass("strikethrough");
+        if ($(this).hasClass("strikethrough")) {
+          if (confirm("Then why did you cross it off?!?! Are you sure?")) {
+            $(this).removeClass("strikethrough");
+          }
+        } else {
+          $(this).addClass("strikethrough");
+        }
       }
     });
 
     $(".arrow").last().click(function(){
       if($(this).parent().children(".note").hasClass("hidden")){
         $(this).parent().children(".note").removeClass("hidden");
-        $(this).parent().children(".arrow").html("  notes &#9660;");
+        $(this).parent().children(".arrow").html("     hide note &#9650;");
       } else {
         $(this).parent().children(".note").addClass("hidden");
-        $(this).parent().children(".arrow").html("  notes >");
+        $(this).parent().children(".arrow").html("     show note &#9660;");
       }
     });
+  });
+
+  $("#removeBtn").click(function() {
+    if ($("html").hasClass("deleteCursor")) {
+      $("html").removeClass("deleteCursor");
+      $(".instructions").hide();
+    } else {
+      $("html").addClass("deleteCursor");
+      $("html").removeClass("noteCursor");
+      $(".instructions").show();
+      $(".noteInstructions").hide();
+
+    }
+  });
+  $("#noteBtn").click(function() {
+    if ($("html").hasClass("noteCursor")) {
+      $("html").removeClass("noteCursor");
+      $(".noteInstructions").hide();
+    } else {
+      $("html").addClass("noteCursor");
+      $("html").removeClass("deleteCursor");
+      $(".noteInstructions").show();
+      $(".instructions").hide();
+    }
   });
 });
 
