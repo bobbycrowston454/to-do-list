9dd9e1e7eec6d00f65ec9185b9c57e3b3d36e49d
Bobby Crowston and Ryan Apking <ryan@>
2016-08-22 11:47:17 -0700

    add notes that can be hidden and displayed by clicking on note

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //back end logic
-function Task(item, important){
+function Task(item, important, note){
   this.item = item;
   this.important = important;
+  this.note = note;
 }
 //user interface
 $(document).ready(function(){
@@ -9,15 +10,26 @@ $(document).ready(function(){
     event.preventDefault();
     var itemInput = $("#itemInput").val();
     var important = $("#importantCheck").is(':checked');
-    var newTask = new Task(itemInput, important);
-    $("#results").append("<li><span class='item'>" + newTask.item + "</span></li>");
+    var note = $("#noteInput").val();
+    var newTask = new Task(itemInput, important, note);
+    var listItemString = "<span class='item'>" + newTask.item + "  </span>";
+    if (note!== "") {
+      listItemString = listItemString + "<span class='arrow'>  notes ></span>";
+      listItemString = listItemString + "<br><span class='note hidden'>" + newTask.note + "</span>";
+    }
+    $("#results").append("<li>" + listItemString + "</li>");
+
+    $("#itemInput").val("");
+    $("#noteInput").val("");
+    $("#importantCheck").prop('checked', false);
+
     if (newTask.important) {
       $(".item").last().addClass("bold");
     };
-    
+
     $(".item").last().click(function() {
       if ($(this).hasClass("strikethrough")) {
-        if (confirm("Are you sure")) {
+        if (confirm("Then why did you cross it off?!?! Are you sure?")) {
           $(this).removeClass("strikethrough");
         }
       } else {
@@ -25,5 +37,16 @@ $(document).ready(function(){
       }
     });
 
+    $(".arrow").last().click(function(){
+      if($(this).parent().children(".note").hasClass("hidden")){
+        $(this).parent().children(".note").removeClass("hidden");
+        $(this).parent().children(".arrow").html("  notes &#9660;");
+      } else {
+        $(this).parent().children(".note").addClass("hidden");
+        $(this).parent().children(".arrow").html("  notes >");
+      }
+    });
   });
 });
+
+// &#9660;
